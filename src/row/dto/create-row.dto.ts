9ba651ae1548cec1dccdee11c8dbd72d8a1c4544
dimas <chiki.briki2@gmail.com>
2024-12-17 00:55:47 +0200

    value.trimm

--- src/row/dto/create-row.dto.ts
+++ src/row/dto/create-row.dto.ts
@@ -3,36 +3,36 @@ import {IsUrl, IsOptional, IsString, isURL, Length, IsNumber, Min, IsEmail, Matc
 import {Transform} from "class-transformer";
 
 export class CreateRowDto {
-   @ApiProperty({example: '1', description: 'Number of row google sheets'})
-   @Transform(({value}) => isNaN(parseInt(value)) ? 1 : parseInt(value),)
-   @IsNumber({}, {message: 'Face be Number'})
-   @Min(1)
-   row_sheets: number;
+    @ApiProperty({example: '1', description: 'Number of row google sheets'})
+    @Transform(({value}) => isNaN(parseInt(value)) ? 1 : parseInt(value),)
+    @IsNumber({}, {message: 'Face be Number'})
+    @Min(1)
+    row_sheets: number;
 
-   @Transform(({value}) => {
-      if( value.trim() === '') return null;
-      return value;
-   })
-   @ApiProperty({example: 'A', description: 'Name column of google sheets'})
-   @IsString({message: 'Name column, should be string'})
-   @Matches(/^[A-Z]$/, { message: 'Column name must be a single uppercase letter (A-Z)' })
-   readonly column_sheets: string;
+    @Transform(({value}) => {
+        if (value === '') return null;
+        return value;
+    })
+    @ApiProperty({example: 'A', description: 'Name column of google sheets'})
+    @IsString({message: 'Name column, should be string'})
+    @Matches(/^[A-Z]$/, {message: 'Column name must be a single uppercase letter (A-Z)'})
+    readonly column_sheets: string;
 
-   @ApiProperty({example: 'some text', description: 'text in cell'})
-   @IsString({message: 'text in cell, should be string'})
-   @Length(0, 550, {message: 'Text in cell has got min length 0  and max length 550'})
-   readonly text: string;
+    @ApiProperty({example: 'some text', description: 'text in cell'})
+    @IsString({message: 'text in cell, should be string'})
+    @Length(0, 550, {message: 'Text in cell has got min length 0  and max length 550'})
+    readonly text: string;
 
-   @Transform(({value}) => {
-      return value.toString() === 'true';
-   })
-   @ApiProperty({example: true, description: 'empty or no row'})
-   @IsOptional()
-   readonly empty?: boolean;
+    @Transform(({value}) => {
+        return value.toString() === 'true';
+    })
+    @ApiProperty({example: true, description: 'empty or no row'})
+    @IsOptional()
+    readonly empty?: boolean;
 
-   @IsEmail()
-   @ApiProperty({example: "temp1@gmail.com", description: 'Email example'})
-   @IsOptional()
-   readonly user_email?: string;
+    @IsEmail()
+    @ApiProperty({example: "temp1@gmail.com", description: 'Email example'})
+    @IsOptional()
+    readonly user_email?: string;
 
 }
